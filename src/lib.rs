8746e654378bb1da1f1742a1377e99b597362c1f
Glubiz <josj@solskov-jensen.dk>
2024-02-15 07:06:35 +0100

    Adding traits implementations

--- src/lib.rs
+++ src/lib.rs
@@ -44,6 +44,53 @@ pub trait LogExt {
     fn timestamp(self, timestamp: SystemTime) -> Self;
 }
 
+impl LogExt for Log {
+    fn new() -> Self {
+        Self {
+            user_id: None,
+            system: None,
+            file: None,
+            line: None,
+            message: None,
+            source: None,
+            log_level: None,
+            timestamp: SystemTime::now(),
+        }
+    }
+    fn user_id(mut self, user_id: String) -> Self {
+        self.user_id = Some(user_id);
+        self
+    }
+    fn system(mut self, system: String) -> Self {
+        self.system = Some(system);
+        self
+    }
+    fn file(mut self, file: String) -> Self {
+        self.file = Some(file);
+        self
+    }
+    fn line(mut self, line: u32) -> Self {
+        self.line = Some(line);
+        self
+    }
+    fn message(mut self, message: String) -> Self {
+        self.message = Some(message);
+        self
+    }
+    fn source(mut self, source: Source) -> Self {
+        self.source = Some(source);
+        self
+    }
+    fn log_level(mut self, log_level: LogType) -> Self {
+        self.log_level = Some(log_level);
+        self
+    }
+    fn timestamp(mut self, timestamp: SystemTime) -> Self {
+        self.timestamp = timestamp;
+        self
+    }
+}
+
 pub trait SourceExt {
     fn new() -> Self;
     fn name(self, name: String) -> Self;
@@ -51,4 +98,36 @@ pub trait SourceExt {
     fn environment(self, environment: String) -> Self;
     fn service(self, service: String) -> Self;
     fn ip(self, ip: String) -> Self;
+}
+
+impl SourceExt for Source {
+    fn new() -> Self {
+        Self {
+            name: None,
+            version: None,
+            environment: None,
+            service: None,
+            ip: None,
+        }
+    }
+    fn name(mut self, name: String) -> Self {
+        self.name = Some(name);
+        self
+    }
+    fn version(mut self, version: String) -> Self {
+        self.version = Some(version);
+        self
+    }
+    fn environment(mut self, environment: String) -> Self {
+        self.environment = Some(environment);
+        self
+    }
+    fn service(mut self, service: String) -> Self {
+        self.service = Some(service);
+        self
+    }
+    fn ip(mut self, ip: String) -> Self {
+        self.ip = Some(ip);
+        self
+    }
 }
